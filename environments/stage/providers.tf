42a71af3f379ab1e90c6ea1ffed5f32d5480aa03
admin <admin@MacBookPro.lan>
2025-02-25 21:15:04 -0600

    removing provider

--- environments/stage/providers.tf
+++ environments/stage/providers.tf
@@ -1,17 +1,6 @@
 # provider block for target account
+
+# provider block for target account
 provider "aws" {
   region = var.region
-
-    assume_role {
-    role_arn = "arn:aws:iam::${var.target_account_id}:role/github-actions-oidc-devsecops-terraform-sctv-loki-cdn-${var.env}"
-  }
-}
-
-
-provider "awscc" {
-  alias = "awscc-provider"
-  region = var.region
-  assume_role = {
-    role_arn     = "arn:aws:iam::${var.target_account_id}:role/github-actions-oidc-devsecops-terraform-sctv-loki-cdn-${var.env}"
-}
 }
