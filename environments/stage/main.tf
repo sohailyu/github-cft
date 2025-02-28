38c47f41ed5f196d793e25ef958784efa539ed7b
admin <admin@MacBookPro.lan>
2025-02-27 18:08:48 -0600

    adding policy

--- environments/stage/main.tf
+++ environments/stage/main.tf
@@ -6,3 +6,26 @@ resource "aws_s3_bucket" "test_bucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_policy" "test_bucket_policy" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::123456789012:user/specific-user"
+      },
+      "Action": "s3:*",
+      "Resource": [
+        "arn:aws:s3:::test0990890",
+        "arn:aws:s3:::test0990890/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
