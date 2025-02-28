0538c4613cbd011352bd588455f752aaaea354db
admin <admin@MacBookPro.lan>
2025-02-27 21:55:28 -0600

    adding new module

--- environments/stage/main.tf
+++ environments/stage/main.tf
@@ -1,12 +1,5 @@
 module "new-module" {
   source                    = "../../modules/new-module"
-  env                       = var.env
-  general_tags              = var.general_tags
-  distribution_id           = var.distribution_id
-  pagerduty_integration_key = var.pagerduty_integration_key
-  slack_channel_id          = var.slack_channel_id
-  slack_workspace_id        = var.slack_workspace_id
-  alarms = var.alarms
   providers = {
     awscc.awsccnew =  awscc.awscc-provider
   }
