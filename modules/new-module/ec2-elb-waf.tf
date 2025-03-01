2708cd56ffd13281b2edbc28bf029d31c404c669
admin <admin@MacBookPro.lan>
2025-03-01 14:22:13 -0600

    modifying subnet az

--- modules/new-module/ec2-elb-waf.tf
+++ modules/new-module/ec2-elb-waf.tf
@@ -8,7 +8,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-west-2a"
 }
 
 resource "aws_security_group" "elb_sg" {
