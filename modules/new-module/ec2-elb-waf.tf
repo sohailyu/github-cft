c9b8ad32c2476907edc802f512e322139c7546b3
admin <admin@MacBookPro.lan>
2025-03-01 14:41:29 -0600

    adding IGW and RT

--- modules/new-module/ec2-elb-waf.tf
+++ modules/new-module/ec2-elb-waf.tf
@@ -34,7 +34,7 @@ resource "aws_subnet" "public-1" {
   availability_zone       = "us-west-2b"
 }
 
-resource "aws_route_table_association" "public_assoc" {
+resource "aws_route_table_association" "public_assoc-1" {
   subnet_id      = aws_subnet.public-1.id
   route_table_id = aws_route_table.public_rt.id
 }
