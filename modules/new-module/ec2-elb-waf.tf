515e425dde5c0e034d68f038bcfa73290d5c9200
admin <admin@MacBookPro.lan>
2025-03-01 14:38:24 -0600

    adding IGW and RT

--- modules/new-module/ec2-elb-waf.tf
+++ modules/new-module/ec2-elb-waf.tf
@@ -4,11 +4,27 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-west-2a"
+}
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_subnet" "public-1" {
@@ -16,6 +32,11 @@ resource "aws_subnet" "public-1" {
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-west-2b"
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public-1.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_security_group" "elb_sg" {
